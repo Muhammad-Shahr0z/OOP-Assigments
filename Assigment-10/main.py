8e87ae32eb47295ccc3c2df0d3e6e132b1c2d86e
Muhammad Shahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2025-05-14 18:11:24 +0500

    Added

---
+++ Assigment-10/main.py
@@ -0,0 +1,19 @@
+# 10. Instance Methods
+# Assignment:
+# Create a class Dog with instance variables name and breed. Add an instance method bark() that prints a message including the dog's name.
+
+
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} is barking!")
+
+
+dog1 = Dog("Rex", "German Shepherd")
+dog1.bark()
+
+dog2 = Dog("Max", "Husky")
+dog2.bark()
